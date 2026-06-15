1348fffe256dff6ea331e259035fb3ca209e1eb3
bernardoraach <bernardo.rach@gmail.com>
2026-06-15 15:50:57 -0300

    rotas api

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 
-# print("--- Sistema de Geração de Formulários")
-
-# nome= input("Digite seu nome: ")
-# cpf = input("Digite o seu CPF: ")
-# nome_filho = input("Nome do filho: ")
-# renda = input("Renda média no valor de: ")
-
-# #testando
-# print("\n Dados Recebidos com Sucesso")
-# print(f"\n Nome informado: {nome}")
-# print(f"\n CPF informado: {cpf}")
-# print(f"\n Filho informado: {nome_filho}")
-# print(f"\n Renda mensal aproximada de: {renda}")
-
-
 #dicionários e funções
-from typing import final
+# from typing import final
+
+# IMPORTADO flask import para trasformar em um servidor web
+#no terminal digitado : pip3 install flask
+
+# from crypt import methods
+# from pydoc import doc
+from flask import Flask, request, jsonify;
+
+app = Flask(__name__)
 
 
 def gerar_doc(dados, tipo): 
@@ -46,37 +40,40 @@ def salvar (arquivo, conteudo):
     with open(arquivo, "w", encoding="utf-8") as arquivo:
         arquivo.write(conteudo)
         print(f"\nArquivo salvo na pasta '{arquivo}'")
+
+#criando rota de API
+
+@app.route('/gerar-documento', methods=['POST'])
+def api_gerar_documento():
+
+    requisicao = request.get_json()
+
+    dados_pessoa = requisicao.get('pessoa')
+    selecionar = requisicao.get('opcao_formulario')
+
+    doc_final = gerar_doc(dados_pessoa, selecionar)
+
+
+    if "Erro:" not in doc_final:
+        nome_arquivo = f"documento_{dados_pessoa['nome'].lower().replace(' ', '-')}.txt"
+        salvar(nome_arquivo, doc_final)
+
+        return jsonify({
+            "status": "sucesso",
+            "documento": doc_final,
+            "arquivo_salvo": nome_arquivo
+        }), 200
+
+    else: 
+        return jsonify({
+            "status": "erro",
+            "mensagem": doc_final
+        }), 400
+
     
-# coleta de dados
-# criando dicionário pessoa
+print(f"O valor de __name__ é: {__name__}"
 
-pessoa = {}
+)
+if __name__ == '__main__':
+        app.run(debug=True, port=5000)
 
-pessoa['nome'] = input("Nome completo: ")
-pessoa['cpf'] = input("CPF: ")
-pessoa['filho'] = input("Nome do filho (caso não tiver, pressione a tecla enter): ")
-pessoa['endereco'] = input("Endereco completo: ")
-pessoa['renda'] = input("Renda mensal: ")
-
-# usuário escolhendo formulário
-print("\n[1] Demissão | [2] Responsáveis | [3] Residência | [4] Renda")
-selecionar= input("Digite o número da opção: ")
-
-
-# print("\nSelecione o documento que deseja gerar: ")
-# print("[1] Pedido de demissão ")
-# print("[2] Declaração de Responsáveis")
-# print("[4] Declaração de Residência")
-# print("[3] Declaração de Renda")
-
-
-doc_final = gerar_doc(pessoa, selecionar)
-
-print("\n-----------DOCUMENTO GERADO-----------")
-print(doc_final)
-print("----------------------")
-
-if "Erro: " not in doc_final:
-    nome_arquivo = f"documento_{pessoa['nome'].lower().replace(' ', '-')}.txt"
-
-salvar(nome_arquivo, doc_final)
