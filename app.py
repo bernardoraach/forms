a53140bff569c4fedefcaacf981e31e8e26add43
bernardoraach <bernardo.rach@gmail.com>
2026-06-15 11:30:31 -0300

    inicio projeto

---
+++ app.py
@@ -0,0 +1,12 @@
+
+print("--- Sistema de Geração de Formulários")
+
+nome= input("Digite seu nome: ")
+cpf = input("Digite o seu CPF: ")
+nome_filho = input("Nome do filho: ")
+
+#testando
+print("\n Dados Recebidos com Sucesso")
+print(f"\n Nome informado: {nome}")
+print(f"\n CPF informado: {cpf}")
+print(f"\n Filho informado: {nome_filho}")
